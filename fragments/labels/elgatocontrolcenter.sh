c7e70a48aafa206e16cc346f5c5cc2a6a1dd748b
ngs-administration <admins@enginsight.com>
2026-06-29 11:19:15 +0200

    Update elgatocontrolcenter.sh

--- fragments/labels/elgatocontrolcenter.sh
+++ fragments/labels/elgatocontrolcenter.sh
@@ -1,11 +1,7 @@
 elgatocontrolcenter)
     name="Elgato Control Center"
     type="zip"
-    appName="Elgato Control Center"
-    appNewVersion=$(curl -sfL \
-        -H "User-Agent: ElgatoControlCenter/1.8.2 CFNetwork/1494.0.7 Darwin/23.4.0" \
-        "https://gc-updates.elgato.com/macos/eccm-update/stable/app-version-check.json" \
-        | grep -o '"Version":"[^"]*"' | head -1 | cut -d'"' -f4)
-    downloadURL="https://edge.elgato.com/egc/macos/eccm/${appNewVersion}/ElgatoControlCenter-${appNewVersion}.20643.zip"
+    downloadURL="https://edge.elgato.com/egc/macos/eccm/1.8.2/ElgatoControlCenter-1.8.2.20643.zip"
+    appNewVersion=""
     expectedTeamID="Y93VXCB8Q5"
     ;;
